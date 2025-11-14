d69a295cf9cb11e2f5b9ad533d2ecdfa17998922
Wazard <34705162+Wazard@users.noreply.github.com>
2025-11-14 11:41:17 +0100

    introduction to functions

--- 2025-11-13/data-structures_exercises.py
+++ 2025-11-13/data-structures_exercises.py
@@ -3,6 +3,7 @@
 #----
 
 
+
 def words_to_int(s):
     ones = {"zero": 0, "one": 1, "two": 2, "three": 3, "four": 4,"five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9}
     teens = {"ten": 10, "eleven": 11, "twelve": 12, "thirteen": 13,"fourteen": 14, "fifteen": 15, "sixteen": 16,"seventeen": 17, "eighteen": 18, "nineteen": 19}
---
+++ 2025-11-14/function-exercises.py
@@ -0,0 +1,43 @@
+#----
+# EXERCISE 1
+#----
+import random
+
+
+def guess_the_number():
+    n:int = int(input("Try and guess a number between 0 and 100"))
+    result =("Correct","Incorrect")[n == random.randint(0,100)]
+    print(f"Your answer is: {result}")
+    return
+
+
+#----
+# EXERCISE 2, 3
+#----
+
+
+def fib(n):
+    if n==0:
+        return 0
+    elif n==1:
+        return 1
+    return fib(n-1)+fib(n-2)
+
+def fib_printer():
+    record = []
+    while True:
+        temp_record = []
+        n:int = int(input("Choose a number: "))
+        for i in range(n+2):
+            fib_i = fib(i)
+            if fib_i > n:
+                break
+            print(fib_i)
+            temp_record.append(fib_i)
+        record.append(temp_record)
+        if input("do you wish to start over?(Y/N): ").lower() == "n":
+            break
+    print(f"Alle records: {record}")
+
+
+fib_printer()
