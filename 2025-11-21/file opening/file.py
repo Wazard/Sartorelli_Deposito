82472f4b24e79887b1f3bcb99a62372114674a81
Wazard <34705162+Wazard@users.noreply.github.com>
2025-11-24 11:45:23 +0100

    filter exercise

---
+++ 2025-11-21/file opening/file.py
@@ -0,0 +1,4 @@
+file = open("my_file.txt", "w")
+new_content = "DIOMERDA"
+file.write(new_content)
+file.close()
---
+++ 2025-11-24/filter_ex.py
@@ -0,0 +1,13 @@
+arr = [1,2,3,4,5,10,7,8,9,10]
+
+result = list(
+    map(lambda i: arr[i], filter(lambda i: arr[i] + arr[i+1] > arr[i+2], range(len(arr)-2)))
+)
+
+# range gives us a list of all indices but last 2 (len(arr)-2)
+# condition: arr[i]+arr[i+1]>arr[i+2]
+# then each filtered index is mapped to arr
+
+
+
+print(result)
