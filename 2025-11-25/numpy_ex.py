eb06c9d1fef0902e6b83c754c6bd3f26bab9612c
Wazard <34705162+Wazard@users.noreply.github.com>
2025-11-25 14:43:29 +0100

    numpy

--- 2025-11-25/numpy_ex.py
+++ 2025-11-25/numpy_ex.py
@@ -21,14 +21,24 @@ mat_diag = np.diag(mat)
 sum_mat_diag = np.sum(mat_diag)
 
 print(
-    f"COL2: {mat_col_2}",
-    f"ROW3: {mat_row_3}",
-    f"DIAGL: {mat_diag}",
+    f"COL2: {mat_col_2}\n",
+    f"ROW3: {mat_row_3}\n",
+    f"DIAGL: {mat_diag}\n",
     f"DIAG_SUM: {sum_mat_diag}"
     )
 
 ## EXERCISE 3 ##
 
 mat_0 = np.random.randint(10,51,size=(4,4))
-mat_0_indexes = mat_0[1::2]
 
+mask = [(0,1),(1,3),(2,2),(3,0)]
+mat_0_indexes = np.array([mat_0[r, c] for r, c in mask])
+mat_0_odd_rows = mat_0[1::2]
+mat_0[mask] += 10
+
+print(
+    f"MAT:\n {mat_0}\n",
+    f"INDEXES: {mat_0_indexes}\n",
+    f"ODD_ROWS:\n{mat_0_odd_rows}\n",
+    f"BROAD_MAT:\n{mat_0}"
+)
